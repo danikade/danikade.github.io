e74df477f27a00d5a4f5826c99b36adb985cdc20
akunnya.danikade@gmail.com <151298dani>
2018-11-26 19:03:27 +0700

    index.html

---
+++ form.js
@@ -0,0 +1,10 @@
+function get_nama()
+        {
+            var tampung_nama = document.getElementById('text').value;
+            if(tampung_nama == ""){
+                alert("Jangan dikosongi bang, susah bikinnya");
+            } else {
+                alert("Nah gitu di lengkapin dong, SUKSES GAN!!	");
+            }
+        
+        }
